0edf4bdd74bb7bc1ce11ddbfe42dd3d8f4bdc1f2
Dei Gott <dollyetal@proton.me>
2023-09-14 16:02:35 +0100

    War es Stress? Naturlich

--- p1/scripts/k3s_controller_install.sh
+++ p1/scripts/k3s_controller_install.sh
@@ -3,11 +3,6 @@
 # Setup path of the controller node token.
 NODE_TOKEN="/var/lib/rancher/k3s/server/node-token"
 
-# Configure centos appstream mirrorlist
-cd /etc/yum.repos.d/
-sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
-
 # Configuring shared folder
 sudo mkdir /vagrant/.configs
       
