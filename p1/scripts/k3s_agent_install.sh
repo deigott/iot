c0b4badbd7ed7c57615f4061e8b1a2561fc13da6
Dei Gott <dollyetal@proton.me>
2023-09-14 09:49:35 +0100

    Setup alias for kubetcl.

--- p1/scripts/k3s_agent_install.sh
+++ p1/scripts/k3s_agent_install.sh
@@ -4,7 +4,11 @@
 K3S_TOKEN_FILE="/vagrant/.configs/master-token"
 
 # Install k3s and configure a worker node.
-curl -sfL https://get.k3s.io | K3S_TOKEN_FILE=${K3S_TOKEN_FILE} K3S_URL="https://192.168.56.110:6443" INSTALL_K3S_EXEC="--flannel-iface=eth1" sh - && echo "(+) Worker is configured."
+curl -sfL https://get.k3s.io | K3S_TOKEN_FILE=${K3S_TOKEN_FILE} K3S_URL="https://192.168.56.110:6443" INSTALL_K3S_EXEC="agent --flannel-iface=eth1" sh - && echo "[INFO] Worker is configured."
 
 # Remove controller node token after configuring worker node.
-sudo rm -rf /vagrant/.configs && echo "(+) Token Removed."
+sudo rm -rf /vagrant/.configs && echo "[INFO] Token Removed."
+
+# Setup alias for kubectl to k
+echo "[SETUP] : initiat aliases for all machine users "
+echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
