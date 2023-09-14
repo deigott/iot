1bc3997ac10ad51df902146cae54f139da61b86c
Dei Gott <dollyetal@proton.me>
2023-09-14 09:45:23 +0100

    Adding controller provision script.

---
+++ p1/scripts/k3s_controller_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Setup path of the controller node token.
+NODE_TOKEN="/var/lib/rancher/k3s/server/node-token"
+
+# Configuring shared folder
+sudo mkdir /vagrant/.configs
+      
+curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode 644 --flannel-iface=eth1 && echo "(+) Master is configured."
+
+cp ${NODE_TOKEN} /vagrant/.configs/master-token && echo "(+) Token is shared."
