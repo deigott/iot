e1be2f93144e6993fb9a56b57dda557353d8b52d
Dei Gott <dollyetal@proton.me>
2023-09-14 13:45:51 +0100

    Removing ubuntu commands.

--- p1/scripts/k3s_controller_install.sh
+++ p1/scripts/k3s_controller_install.sh
@@ -3,6 +3,7 @@
 # Setup path of the controller node token.
 NODE_TOKEN="/var/lib/rancher/k3s/server/node-token"
 
+
 # Configuring shared folder
 sudo mkdir /vagrant/.configs
       
@@ -13,3 +14,5 @@ cp ${NODE_TOKEN} /vagrant/.configs/master-token && echo "[INFO] Token is shared.
 # Setup alias for kubectl to k
 echo "[SETUP] : initiat aliases for all machine users "
 echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
+
+sudo yum install net-tools -y
