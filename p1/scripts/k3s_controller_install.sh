75b01c7ebd36fca66facb9dfac2b29e4ad7ff93d
Dei Gott <dollyetal@proton.me>
2023-09-14 09:49:23 +0100

    Setup alias for kubectl.

--- p1/scripts/k3s_controller_install.sh
+++ p1/scripts/k3s_controller_install.sh
@@ -6,6 +6,10 @@ NODE_TOKEN="/var/lib/rancher/k3s/server/node-token"
 # Configuring shared folder
 sudo mkdir /vagrant/.configs
       
-curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode 644 --flannel-iface=eth1 && echo "(+) Master is configured."
+curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode 644 --flannel-iface=eth1 && echo "[INFO] Master is configured."
 
-cp ${NODE_TOKEN} /vagrant/.configs/master-token && echo "(+) Token is shared."
+cp ${NODE_TOKEN} /vagrant/.configs/master-token && echo "[INFO] Token is shared."
+
+# Setup alias for kubectl to k
+echo "[SETUP] : initiat aliases for all machine users "
+echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
