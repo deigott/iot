cced77cb76096365c245266f2a39973b95e6db2a
Dei Gott <dollyetal@proton.me>
2023-09-20 09:33:46 +0100

    Adding communication between agent and worker node.

--- p1/scripts/k3s_controller_install.sh
+++ p1/scripts/k3s_controller_install.sh
@@ -9,6 +9,7 @@ sudo mkdir /vagrant/.configs
 curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode 644 --flannel-iface=eth1 && echo "[INFO] Master is configured."
 
 cp ${NODE_TOKEN} /vagrant/.configs/master-token && echo "[INFO] Token is shared."
+sed 's/127.0.0.1/192.168.56.110/g' /etc/rancher/k3s/k3s.yaml > /vagrant/.configs/k3s.yml
 
 # Setup alias for kubectl to k
 echo "[SETUP] : initiat aliases for all machine users "
